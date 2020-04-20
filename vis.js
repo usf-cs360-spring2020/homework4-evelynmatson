f43d69a4ba146990ba06e338663bb9158a47bc85
evelynmatson <ematson@dons.usfca.edu>
2020-04-20 11:27:24 -0700

    visualizing something messed up

--- vis.js
+++ vis.js
@@ -1,3 +1,6 @@
+let svg;
+let colorScale;
+
 /**
  * Prepare things, load the data, then draw the visualization
  */
@@ -5,6 +8,7 @@ function prepVis() {
 
 
     // TODO prepare things, set up
+    svg = d3.select('svg#vis_svg');
 
 
 
@@ -77,7 +81,32 @@ function drawVis(data) {
     });
     console.log('root with calculations', stratifiedRoot);
 
-    // TODO draw something visual-ish
+    //  Actually draw something
+    colorScale = d3.scaleSequential([stratifiedRoot.height, 0], d3.interpolateViridis);
+
+    let module = stratifiedRoot.copy();
+    let pad = 0;
+    let diam = 400;
+    let layout = d3.tree().size([2*Math.PI, (diam/2) - pad]);
+    layout(module);
+
+    module.each(function(node) {
+        node.theta = node.x;
+        node.r = node.y;
+
+        let point = toCartesian(node.r, node.theta);
+        node.x = point.x;
+        node.y = point.y;
+    });
+
+    let width = 400;
+    let height = 400;
+    let plot = svg.append('g')
+        .attr('id', 'plot1')
+        .attr('transform', translate(width / 2, height / 2));
+
+    drawLinks(plot.append('g'), module.links(), straightLine);
+    drawNodes(plot.append('g'), module.descendants(), true);
 }
 
 /**
@@ -109,6 +138,7 @@ function convertRow(row) {
     return toReturn;
 }
 
+// MY HELPERS
 /**
  * My own filter unique function. Unique based on an identifier calculated using func.
  * @param stuff the list of things to filter
@@ -129,6 +159,178 @@ function filterUnique(stuff, func) {
     });
 
     return newStuff;
+}
+
+
+// SOPHIE'S HELPERS
+
+// Sophie's straight line generator
+let straightlinehelper = d3.line()
+        .curve(d3.curveLinear)
+        .x(d => d.x)
+        .y(d => d.y);
+let straightLine = function(node) {
+    return straightlinehelper([node.source, node.target]);
+};
+
+/**
+ * Sophie's helpful translate method.
+ * @param x horizontal amount to translate
+ * @param y vertical amount to translate
+ * @returns {string} a string to be used for translating
+ */
+function translate(x, y) {
+    return 'translate(' + String(x) + ',' + String(y) + ')';
+}
+
+/**
+ * Sophie's helpful polar/cartesian converter
+ * @param r the radial value
+ * @param theta the angle
+ * @returns {{x: number, y: number}}
+ */
+function toCartesian(r, theta) {
+    return {
+        x: r * Math.cos(theta),
+        y: r * Math.sin(theta)
+    };
+}
+
+/**
+ * Sophie's helpful drawlinks method
+ * @param g where to draw them
+ * @param links the links to draw
+ * @param generator the generator to use
+ */
+function drawLinks(g, links, generator) {
+    let paths = g.selectAll('path')
+        .data(links)
+        .enter()
+        .append('path')
+        .attr('d', generator)
+        .attr('class', 'link');
+}
+
+/**
+ * Sophie's helpful drawnodes method
+ * @param g where to draw them
+ * @param nodes the nodes to use
+ * @param raise whether to raise them up
+ */
+function drawNodes(g, nodes, raise) {
+    let r = 5;
+
+    let circles = g.selectAll('circle')
+        .data(nodes, node => node.data.name)
+        .enter()
+        .append('circle')
+        .attr('r', d => d.r ? d.r : r)
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('id', d => d.data.name)
+        .attr('class', 'node')
+        .style('fill', d => colorScale(d.depth));
+
+    setupEvents(g, circles, raise);
+}
+
+/**
+ * Sophie's helpful setup events method
+ * @param g where to draw
+ * @param selection the selected element
+ * @param raise whether to raise them
+ */
+function setupEvents(g, selection, raise) {
+    selection.on('mouseover.highlight', function(d) {
+        // https://github.com/d3/d3-hierarchy#node_path
+        // returns path from d3.select(this) node to selection.data()[0] root node
+        let path = d3.select(this).datum().path(selection.data()[0]);
+
+        // select all of the nodes on the shortest path
+        let update = selection.data(path, node => node.data.name);
+
+        // highlight the selected nodes
+        update.classed('selected', true);
+
+        if (raise) {
+            update.raise();
+        }
+    });
+
+    selection.on('mouseout.highlight', function(d) {
+        let path = d3.select(this).datum().path(selection.data()[0]);
+        let update = selection.data(path, node => node.data.name);
+        update.classed('selected', false);
+    });
+
+    // show tooltip text on mouseover (hover)
+    selection.on('mouseover.tooltip', function(d) {
+        showTooltip(g, d3.select(this));
+    });
+
+    // remove tooltip text on mouseout
+    selection.on('mouseout.tooltip', function(d) {
+        g.select("#tooltip").remove();
+    });
+}
+
+/**
+ * Sophie's helpful showtooltip method
+ * @param g where to put new elements in the DOM
+ * @param node the node to show the tooltip about
+ */
+function showTooltip(g, node) {
+    let gbox = g.node().getBBox();     // get bounding box of group BEFORE adding text
+    let nbox = node.node().getBBox();  // get bounding box of node
+
+    // calculate shift amount
+    let dx = nbox.width / 2;
+    let dy = nbox.height / 2;
+
+    // retrieve node attributes (calculate middle point)
+    let x = nbox.x + dx;
+    let y = nbox.y + dy;
+
+    // get data for node
+    let datum = node.datum();
+
+    // remove "java.base." from the node name
+    let name = datum.data.name.replace("java\.base\.", "");
+
+    // use node name and total size as tooltip text
+    let text = `${name} (${numberFormat(datum.data.total)}, ${numberFormat(datum.data.leaves)}n)`;
+
+    // create tooltip
+    let tooltip = g.append('text')
+        .text(text)
+        .attr('x', x)
+        .attr('y', y)
+        .attr('dy', -dy - 4) // shift upward above circle
+        .attr('text-anchor', 'middle') // anchor in the middle
+        .attr('id', 'tooltip');
+
+    // it is possible the tooltip will fall off the edge of the
+    // plot area. we can detect when this happens, and set the
+    // text anchor appropriately
+
+    // get bounding box for the text
+    let tbox = tooltip.node().getBBox();
+
+    // if text will fall off left side, anchor at start
+    if (tbox.x < gbox.x) {
+        tooltip.attr('text-anchor', 'start');
+        tooltip.attr('dx', -dx); // nudge text over from center
+    }
+    // if text will fall off right side, anchor at end
+    else if ((tbox.x + tbox.width) > (gbox.x + gbox.width)) {
+        tooltip.attr('text-anchor', 'end');
+        tooltip.attr('dx', dx);
+    }
+
+    // if text will fall off top side, place below circle instead
+    if (tbox.y < gbox.y) {
+        tooltip.attr('dy', dy + tbox.height);
+    }
 }
 
 // Let's do this!
